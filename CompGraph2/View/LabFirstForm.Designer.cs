a3abf56f48ddce94e7a17ae92315f50ffed5f548
Vladimir <vova-gorohov04@mail.ru>
2024-03-01 15:25:25 +0700

    Сделал Транспонирование матриц

--- CompGraph2/View/LabFirstForm.Designer.cs
+++ CompGraph2/View/LabFirstForm.Designer.cs
@@ -32,6 +32,7 @@ namespace CompGraph
             this.TabControl = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.TabControl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,43 +40,50 @@ namespace CompGraph
             // 
             this.TabControl.Controls.Add(this.tabPage1);
             this.TabControl.Controls.Add(this.tabPage2);
+            this.TabControl.Controls.Add(this.tabPage3);
             this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TabControl.Location = new System.Drawing.Point(0, 0);
-            this.TabControl.Margin = new System.Windows.Forms.Padding(4);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(1314, 687);
+            this.TabControl.Size = new System.Drawing.Size(986, 558);
             this.TabControl.TabIndex = 9;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage1.Size = new System.Drawing.Size(1306, 658);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(978, 532);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Matrix Operations";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(1306, 658);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(978, 532);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Vector Module";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(978, 532);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Matrix Transposition";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
             // LabFirstForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1314, 687);
+            this.ClientSize = new System.Drawing.Size(986, 558);
             this.Controls.Add(this.TabControl);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "LabFirstForm";
             this.Text = "LabFirst";
             this.Load += new System.EventHandler(this.LabFirstForm_Load);
@@ -89,6 +97,7 @@ namespace CompGraph
         private System.Windows.Forms.TabPage tabPage1;
         private View.VectorModule vectorModulе1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
 
--- CompGraph2/View/MatrixOperations.Designer.cs
+++ CompGraph2/View/MatrixOperations.Designer.cs
@@ -57,7 +57,7 @@ namespace CompGraph.View
             this.panel1.Controls.Add(this.NLabel);
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(708, 572);
+            this.panel1.Size = new System.Drawing.Size(337, 419);
             this.panel1.TabIndex = 19;
             // 
             // RezultSubtractionButton
@@ -171,7 +171,7 @@ namespace CompGraph.View
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
             this.Name = "MatrixOperations";
-            this.Size = new System.Drawing.Size(873, 682);
+            this.Size = new System.Drawing.Size(353, 435);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
