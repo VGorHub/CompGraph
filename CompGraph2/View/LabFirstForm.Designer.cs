2d45abad6b33271beda25b41740ac292197f9f17
gaershov <159442009+gaershov@users.noreply.github.com>
2024-03-01 18:12:41 +0300

    test

--- CompGraph2/View/LabFirstForm.Designer.cs
+++ CompGraph2/View/LabFirstForm.Designer.cs
@@ -29,12 +29,42 @@ namespace CompGraph
         /// </summary>
         private void InitializeComponent()
         {
-            this.TabControl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.TabControl = new System.Windows.Forms.TabControl();
             this.TabControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(978, 532);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Matrix Transposition";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(978, 532);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Vector Module";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(978, 532);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Matrix Operations";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // TabControl
             // 
@@ -47,36 +77,6 @@ namespace CompGraph
             this.TabControl.SelectedIndex = 0;
             this.TabControl.Size = new System.Drawing.Size(986, 558);
             this.TabControl.TabIndex = 9;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage1.Size = new System.Drawing.Size(978, 532);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Matrix Operations";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage2.Size = new System.Drawing.Size(978, 532);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Vector Module";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(978, 532);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Matrix Transposition";
-            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // LabFirstForm
             // 
@@ -93,11 +93,11 @@ namespace CompGraph
         }
 
         #endregion
-        private System.Windows.Forms.TabControl TabControl;
-        private System.Windows.Forms.TabPage tabPage1;
         private View.VectorModule vectorModulе1;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl TabControl;
     }
 }
 
