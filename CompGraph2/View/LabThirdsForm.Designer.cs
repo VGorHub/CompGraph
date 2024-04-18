165f6aeb478c636f90c3e4e232f61e5270489091
kpubetko <kpubetko00@mail.ru>
2024-04-18 23:10:24 +0700

    вроде все работает

--- CompGraph2/View/LabThirdsForm.Designer.cs
+++ CompGraph2/View/LabThirdsForm.Designer.cs
@@ -35,9 +35,13 @@
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.labThird_10 = new CompGraph.View.LabThird_10();
+            this.vGor31 = new CompGraph.View.VGor3();
+            this.lab3Stepa1 = new CompGraph.View.Lab3Stepa();
             this.tabControl1.SuspendLayout();
-            this.tabPage4.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -47,50 +51,48 @@
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1115, 807);
+            this.tabControl1.Size = new System.Drawing.Size(836, 656);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Controls.Add(this.lab3Stepa1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage1.Size = new System.Drawing.Size(1107, 778);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(828, 630);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Stepa";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage2.Controls.Add(this.grisha_3Laba_Tab1);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(1107, 778);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(828, 630);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Grisha";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // grisha_3Laba_Tab1
             // 
-            this.grisha_3Laba_Tab1.Location = new System.Drawing.Point(3, 3);
+            this.grisha_3Laba_Tab1.Location = new System.Drawing.Point(2, 2);
+            this.grisha_3Laba_Tab1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grisha_3Laba_Tab1.Name = "grisha_3Laba_Tab1";
-            this.grisha_3Laba_Tab1.Size = new System.Drawing.Size(807, 616);
+            this.grisha_3Laba_Tab1.Size = new System.Drawing.Size(818, 500);
             this.grisha_3Laba_Tab1.TabIndex = 0;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
-            this.tabPage3.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage3.Controls.Add(this.vGor31);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage3.Size = new System.Drawing.Size(1107, 778);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage3.Size = new System.Drawing.Size(828, 630);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Vova";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -98,11 +100,10 @@
             // tabPage4
             // 
             this.tabPage4.Controls.Add(this.labThird_10);
-            this.tabPage4.Location = new System.Drawing.Point(4, 25);
-            this.tabPage4.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage4.Size = new System.Drawing.Size(1107, 778);
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage4.Size = new System.Drawing.Size(828, 630);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Anton";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -110,23 +111,39 @@
             // labThird_10
             // 
             this.labThird_10.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labThird_10.Location = new System.Drawing.Point(4, 4);
+            this.labThird_10.Location = new System.Drawing.Point(3, 3);
+            this.labThird_10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.labThird_10.Name = "labThird_10";
-            this.labThird_10.Size = new System.Drawing.Size(1099, 770);
+            this.labThird_10.Size = new System.Drawing.Size(822, 624);
             this.labThird_10.TabIndex = 0;
+            // 
+            // vGor31
+            // 
+            this.vGor31.Location = new System.Drawing.Point(6, 3);
+            this.vGor31.Name = "vGor31";
+            this.vGor31.Size = new System.Drawing.Size(816, 584);
+            this.vGor31.TabIndex = 0;
+            // 
+            // lab3Stepa1
+            // 
+            this.lab3Stepa1.Location = new System.Drawing.Point(3, 3);
+            this.lab3Stepa1.Name = "lab3Stepa1";
+            this.lab3Stepa1.Size = new System.Drawing.Size(773, 572);
+            this.lab3Stepa1.TabIndex = 0;
             // 
             // LabThirdForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1120, 811);
+            this.ClientSize = new System.Drawing.Size(840, 659);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "LabThirdForm";
             this.Text = "LabThirdForm";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage4.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -140,5 +157,7 @@
         private System.Windows.Forms.TabPage tabPage4;
         private LabThird_10 labThird_10;
         private Grisha_3Laba_Tab grisha_3Laba_Tab1;
+        private Lab3Stepa lab3Stepa1;
+        private VGor3 vGor31;
     }
 }
