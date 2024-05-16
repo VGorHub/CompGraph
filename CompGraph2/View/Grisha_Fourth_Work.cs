b5421e37f3a1314c6d60db3e6862e4b0a035db32
grisha <g.a.ershov@mail.ru>
2024-05-16 15:06:59 +0700

    ggFourth

---
+++ CompGraph2/View/Grisha_Fourth_Work.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// для работы с библиотекой OpenGL
+using Tao.OpenGl;
+// для работы с библиотекой FreeGLUT
+using Tao.FreeGlut;
+// для работы с элементом управления SimpleOpenGLControl
+using Tao.Platform.Windows;
+namespace CompGraph.View
+{
+    public partial class Grisha_Fourth_Work : UserControl
+    {
+        public Grisha_Fourth_Work()
+        {
+            InitializeComponent();
+            // Инициализация контекста окна графического вывода
+            Pr.InitializeContexts();
+        }
+        private void showSolid(int obj)
+        {
+            switch (obj)
+            {
+                case 1:
+                    Glut.glutSolidCone(0.2, 0.75, 16, 8); break; // Конус
+                case 2:
+                    Glut.glutSolidCube(0.75); break; // Куб
+                case 3:
+                    Glut.glutSolidCylinder(0.2, 0.75, 16, 16); break;//Цилиндр 
+                case 4:
+                    Gl.glScaled(0.5, 0.5, 0.5);
+                    Glut.glutSolidDodecahedron(); break; // Додекаэдр
+                case 5:
+                    Glut.glutSolidIcosahedron(); break; // Икосаэдр
+                case 6:
+                    Glut.glutSolidOctahedron(); break; // Октаэдр
+                case 7:
+                    Glut.glutSolidRhombicDodecahedron(); break; // Ромбический додекаэдр
+                case 8:
+                    double[] offset = { 0.0 };
+                    Glut.glutSolidSierpinskiSponge(7, offset, 1); break; // Фрактал Губка Серпиского
+                case 9:
+                    Glut.glutSolidSphere(0.75, 16, 16); break; // Сфера
+                case 10:
+                    Glut.glutSolidTeapot(0.5); break; // Чайник
+                case 11:
+                    Gl.glRotated(180, 0, 1, 0);
+                    Glut.glutSolidTetrahedron(); break; // Тетраэдр
+                case 12:
+                    Glut.glutSolidTorus(0.15, 0.65, 16, 16); break; // Тор
+            }
+        }
+        private void draw(int obj)
+        {
+            // Очистка буфера цвета и буфера глубины
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT | Gl.GL_ACCUM_BUFFER_BIT);
+            // Матрица проецирования
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glRotated(-30, 1, 0, 0);
+            Gl.glRotated(45, 0, 1, 0);
+            // Видовая матрица
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            if (radioButton1.Checked)
+            {
+                // Белый цвет
+                Gl.glColor3f(1, 1, 1);
+                // Выводим glut-примитив в виде каркаса
+                switch (obj)
+                {
+                    case 1:
+                        Glut.glutWireCone(0.2, 0.75, 16, 8); break; // Конус
+                    case 2:
+                        Glut.glutWireCube(0.75); break; // Куб
+                    case 3:
+                        Glut.glutWireCylinder(0.2, 0.75, 16, 16); break; // Цилиндр
+                    case 4:
+                        Gl.glScaled(0.5, 0.5, 0.5);
+                        Glut.glutWireDodecahedron(); break; // Додекаэдр
+                    case 5:
+                        Glut.glutWireIcosahedron(); break; // Икосаэдр
+                    case 6:
+                        Glut.glutWireOctahedron(); break; // Октаэдр
+                    case 7:
+                        Glut.glutWireRhombicDodecahedron(); break; // Ромбический додекаэдр
+                    case 8:
+                        double[] offset = { 0, 0, 0 };
+                        Glut.glutWireSierpinskiSponge(7, offset, 1); break; // Фрактал Губка Серпиского
+                    case 9:
+                        Glut.glutWireSphere(0.75, 16, 16); break; // Сфера
+                    case 10:
+                        Glut.glutWireTeapot(0.5); break; // Чайник
+                    case 11:
+                        Gl.glRotated(180, 0, 1, 0);
+                        Glut.glutWireTetrahedron(); break; // Тетраэдр
+                    case 12:
+                        Glut.glutWireTorus(0.15, 0.65, 16, 16); break; // Тор
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                // Модель освещенности с одним источником цвета
+                float[] light_position = { 10, 10, -30, 0 }; // Координаты источника света
+                float[] lghtClr = { 1, 1, 1, 0 }; // Источник излучает белый цвет
+                float[] mtClr = { 0, 1, 0, 0 }; // Материал зеленого цвета
+                Gl.glPolygonMode(Gl.GL_FRONT, Gl.GL_FILL); // Заливка полигонов
+                Gl.glShadeModel(Gl.GL_SMOOTH); // Вывод с интерполяцией цветов
+                Gl.glEnable(Gl.GL_LIGHTING); // Будем рассчитывать освещенность
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light_position);
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_AMBIENT, lghtClr); // Рассеивание
+                Gl.glEnable(Gl.GL_LIGHT0); // Включаем в уравнение освещенности источник GL_LIGHT0
+                // Диффузионная компонента цвета материала
+                Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, mtClr);
+                // Выводим тонированный glut-примитив
+                showSolid(obj);
+                Gl.glDisable(Gl.GL_LIGHTING); // Будем рассчитывать освещенность
+            }
+            Pr.Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw(1); // Конус
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            draw(2); // Куб
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            draw(3); // Цилиндр
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            draw(4); // Додекаэдр
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            draw(5); // Икосаэдр
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            draw(6); // Октаэдр
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            draw(7); // Ромбический додекаэдр
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            draw(8); // Фрактал Губка Серпиского
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            draw(9); // Сфера
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            draw(10); // Чайник
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            draw(11); // Тетраэдр
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            draw(12); // Тор
+        }
+        private void Grisha_Fourth_Work_Load(object sender, EventArgs e)
+        {
+            // Черный цвет фона
+            Gl.glClearColor(0, 0, 0, 1);
+            // Инициализация Glut
+            Glut.glutInit();
+            // Используем в Glut систему цветов RGB, двойную буферизацию
+            //(экранный и внеэкранный буферы) и буфер глубины
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            // Активизируем тест глубины
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+        }
+    }
+}
--- CompGraph2/View/LabFourthForm.Designer.cs
+++ CompGraph2/View/LabFourthForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CompGraph.View
 {
-    partial class LabThirdForm
+    partial class LabFourthForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,36 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabControl_LabFourth = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.vGor31 = new CompGraph.View.VGor3();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.labThird_10 = new CompGraph.View.LabThird_10();
-            this.tabControl1.SuspendLayout();
-            this.tabPage3.SuspendLayout();
-            this.tabPage4.SuspendLayout();
+            this.tabControl_LabFourth.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tabControl_LabFourth
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Location = new System.Drawing.Point(0, 2);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(828, 656);
-            this.tabControl1.TabIndex = 0;
+            this.tabControl_LabFourth.Controls.Add(this.tabPage1);
+            this.tabControl_LabFourth.Controls.Add(this.tabPage2);
+            this.tabControl_LabFourth.Controls.Add(this.tabPage3);
+            this.tabControl_LabFourth.Controls.Add(this.tabPage4);
+            this.tabControl_LabFourth.Location = new System.Drawing.Point(2, 1);
+            this.tabControl_LabFourth.Name = "tabControl_LabFourth";
+            this.tabControl_LabFourth.SelectedIndex = 0;
+            this.tabControl_LabFourth.Size = new System.Drawing.Size(925, 639);
+            this.tabControl_LabFourth.TabIndex = 0;
             // 
             // tabPage1
             // 
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(820, 630);
+            this.tabPage1.Size = new System.Drawing.Size(917, 613);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Stepa";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -67,74 +63,48 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(820, 630);
+            this.tabPage2.Size = new System.Drawing.Size(917, 613);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Grisha";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            this.tabPage3.Controls.Add(this.vGor31);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(820, 630);
+            this.tabPage3.Size = new System.Drawing.Size(917, 613);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Vova";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // vGor31
-            // 
-            this.vGor31.Location = new System.Drawing.Point(6, 3);
-            this.vGor31.Name = "vGor31";
-            this.vGor31.Size = new System.Drawing.Size(816, 584);
-            this.vGor31.TabIndex = 0;
-            // 
             // tabPage4
             // 
-            this.tabPage4.Controls.Add(this.labThird_10);
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(820, 630);
+            this.tabPage4.Size = new System.Drawing.Size(917, 613);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Anton";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // labThird_10
-            // 
-            this.labThird_10.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labThird_10.Location = new System.Drawing.Point(3, 3);
-            this.labThird_10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.labThird_10.Name = "labThird_10";
-            this.labThird_10.Size = new System.Drawing.Size(814, 624);
-            this.labThird_10.TabIndex = 0;
-            // 
-            // LabThirdForm
+            // LabFourthForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 686);
-            this.Controls.Add(this.tabControl1);
-            this.Name = "LabThirdForm";
-            this.Text = "LabThirdForm";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage3.ResumeLayout(false);
-            this.tabPage4.ResumeLayout(false);
+            this.ClientSize = new System.Drawing.Size(933, 644);
+            this.Controls.Add(this.tabControl_LabFourth);
+            this.Name = "LabFourthForm";
+            this.Text = "LabFourthForm";
+            this.tabControl_LabFourth.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tabControl_LabFourth;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
-        private LabThird_10 labThird_10;
-        private Grisha_3Laba_Tab grisha_3Laba_Tab1;
-        private Lab3Stepa lab3Stepa1;
-        private VGor3 vGor31;
     }
 }
